4dd5d1ddbaad87476f0ddb3a335d48f38f50f342
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2025-11-06 08:26:12 -0600

    Add password generator logic and refine UI consistency
    Implemented JS to generate two password options
    
    Updated HTML naming conventions for clarity
    
    Applied box-sizing: border-box to buttons to prevent size shifts

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let pwd1 = document.querySelector("#pwd1");
+let pwd2 = document.querySelector("#pwd2");
+let generateBtn = document.querySelector("#generateBtn");
+
+generateBtn.addEventListener('click', () =>
+    getRandomPwd(characters));
+
+function getRandomPwd(array) {
+    let charArray1 = [];
+    let charArray2 = [];
+    for (let i = 0; i < 9; i++) {
+        const j = Math.floor(Math.random() * array.length);
+        const x = Math.floor(Math.random() * array.length);
+        charArray1.push(array[j]);
+        charArray2.push(array[x]);
+    };
+    pwd1.textContent = charArray1.join("");
+    pwd2.textContent = charArray2.join("");
+}
+
+
 
 
 
